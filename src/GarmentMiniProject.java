023d07fd029afdc0ce4d56fab2491127a96a2604
nsaakib <sakib2305101847@diu.edu.bd>
2024-11-01 21:12:30 +0600

    init commit

--- src/GarmentMiniProject.java
+++ src/GarmentMiniProject.java
@@ -1,2 +1,126 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+class Garment{
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    public void updateStock(int quantity){
+        this.stockQuantity = quantity;
+    }
+    public double calculateDiscountPrice(double discountPercentage){
+        double discount = this.price * discountPercentage / 100;
+        return discount;
+    }
+
+}
+
+class Fabric {
+
+    public String id;
+    public String type;
+    public String color ;
+    public double pricePerMeter;
+
+    public double calculateCost(double meters){
+        double cost = pricePerMeter * meters;
+        return cost;
+    }
+
+    Garment g = new Garment();
+
+}
+
+class Supplier{
+
+    public String id;
+    public String name;
+    public String contactInfo;
+    private List<Fabric> suppliedFabrics = new ArrayList<>();
+
+    public void addFabric(Fabric fabric){
+        suppliedFabrics.add(fabric);
+    }
+
+
+    List<Fabric> suppliedFabrics(){
+        return suppliedFabrics;
+    }
+
+}
+
+class Order{
+
+    public String orderId;
+    public Date orderDate;
+    public List<Garment> garments = new ArrayList<>() ;
+    public double totalAmount ;
+
+    public void addGarment(Garment garment){
+        garments.add(garment);
+    }
+    public double calculateTotalAmount(){
+        for(Garment g: garments){
+            totalAmount += g.price;
+        }
+        return totalAmount;
+    }
+
+    public void printOrderDetails() {
+
+    }
+
+}
+
+
+class Customer{
+
+    String customerId;
+    String name;
+    String email;
+    String phone;
+
+    void placeOrder(Order order){
+
+    }
+
+    List<Order> viewOrders(){
+        return null;
+    }
+
+}
+
+class Inventory{
+
+    List<Garment> garments ;
+
+
+    void addGarment(Garment garment){
+
+    }
+
+    void removeGarment(String id){
+
+    }
+
+    Garment findGarment(String id){
+
+        return null;
+
+    }
+
+
+}
+
 public class GarmentMiniProject {
+    public static void main(String[] args) {
+
+    }
 }
